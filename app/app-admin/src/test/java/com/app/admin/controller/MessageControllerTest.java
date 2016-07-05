e5558b564ef5fc66300b4bf5c6ca892aff015117
shuzheng <469741414@qq.com>
2016-07-05 18:14:24 +0800

    继承MyBatis Generator，自动生成dao代码

--- app/app-admin/src/test/java/com/app/admin/controller/MessageControllerTest.java
+++ app/app-admin/src/test/java/com/app/admin/controller/MessageControllerTest.java
@@ -1,28 +1,35 @@
 package com.app.admin.controller;
 
-import javax.annotation.Resource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
+import com.app.mapper.UserPOMapper;
+import com.app.model.po.UserPO;
 import com.app.service.MessageService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({
-	"classpath:applicationContext.xml"
+	"classpath:applicationContext.xml",
+	"classpath:applicationContext-jdbc.xml"
 })
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
 public class MessageControllerTest {
 
-	@Resource
+	@Autowired
 	private MessageService messageService;
+	
+	@Autowired
+	private UserPOMapper userMapper;
 	
 	@Test
 	public void index() {
 		System.out.println(messageService.getMessage());
+		UserPO user = userMapper.selectByPrimaryKey(1);
+		System.out.println(user.getNickname());
 	}
 	
 }
