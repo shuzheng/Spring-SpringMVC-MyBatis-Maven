467c5c8198b47b27971cd00c914de810334222f9
shuzheng <469741414@qq.com>
2016-07-09 22:41:31 +0800

    高级查询

--- test/test-dao/src/test/java/com/test/service/UserMapperTest.java
+++ test/test-dao/src/test/java/com/test/service/UserMapperTest.java
@@ -1,5 +1,10 @@
 package com.test.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +12,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
+import com.github.pagehelper.PageHelper;
 import com.test.mapper.UserMapper;
 import com.test.model.User;
+
+import tk.mybatis.mapper.entity.Example;
 
 /**
  * 测试service
@@ -39,6 +47,23 @@ public class UserMapperTest {
 		tempUser.setCtime(System.currentTimeMillis());
 		int count = userMapper.insert(tempUser);
 		System.out.println("插入：" + count + "条数据，id为：" + tempUser.getId());
+		
+		Example example = new Example(User.class);
+		Map<String, Integer> param = new HashMap<String, Integer>();
+		param.put("id", 1);
+		example.createCriteria()
+			.andGreaterThan("id", 0);
+		example.setOrderByClause("id desc");
+		List<User> userList = userMapper.selectByExampleAndRowBounds(example, new RowBounds(0, 10));
+		System.out.println(userList.size());
+		for (User u : userList) {
+			System.out.println(u.getId());
+		}
+		
+		PageHelper.startPage(1, 10);
+		List<User> users = userMapper.select(null);
+		System.out.println(users.size());
+		
 	}
 	
 }
