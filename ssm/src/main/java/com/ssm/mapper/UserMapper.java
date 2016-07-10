12fc4e70589287ff200c7b72a7a66b18719e6ae2
shuzheng <469741414@qq.com>
2016-07-10 18:46:12 +0800

    增加单项目ssm

---
+++ ssm/src/main/java/com/ssm/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.ssm.mapper;
+
+import com.ssm.model.User;
+
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+
+}
---
+++ ssm/src/test/java/com/ssm/service/UserMapperTest.java
@@ -0,0 +1,69 @@
+package com.ssm.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import com.github.pagehelper.PageHelper;
+import com.ssm.mapper.UserMapper;
+import com.ssm.model.User;
+
+import tk.mybatis.mapper.entity.Example;
+
+/**
+ * 测试service
+ * @author shuzheng
+ * @date 2016年7月6日 下午6:07:43
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+	"classpath:applicationContext.xml",
+	"classpath:applicationContext-jdbc.xml"
+})
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+public class UserMapperTest {
+
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Test
+	public void index() {
+		User user = userMapper.selectByPrimaryKey(1);
+		System.out.println(user.getNickname());
+		
+		User tempUser = new User();
+		tempUser.setUsername("test");
+		tempUser.setNickname("测试");
+		tempUser.setPassword("123456");
+		tempUser.setSex(1);
+		tempUser.setCtime(System.currentTimeMillis());
+		int count = userMapper.insert(tempUser);
+		System.out.println("插入：" + count + "条数据，id为：" + tempUser.getId());
+		
+		Example example = new Example(User.class);
+		Map<String, Integer> param = new HashMap<String, Integer>();
+		param.put("id", 1);
+		example.createCriteria()
+			.andGreaterThan("id", 0);
+		example.setOrderByClause("id desc");
+		List<User> userList = userMapper.selectByExampleAndRowBounds(example, new RowBounds(0, 10));
+		System.out.println(userList.size());
+		for (User u : userList) {
+			System.out.println(u.getId());
+		}
+		
+		PageHelper.startPage(1, 10);
+		List<User> users = userMapper.select(null);
+		System.out.println(users.size());
+		
+	}
+	
+}
